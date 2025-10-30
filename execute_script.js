419dbdb6bea9c9ba58912ca9a85f36c7568cbe16
Lunar Mysty <62186470+lunar-mysty@users.noreply.github.com>
2025-10-30 12:26:37 -0400

    Update execute_script.js

--- execute_script.js
+++ execute_script.js
@@ -1,10 +1,10 @@
 /// execute_script.js
 window.onbeforeunload = function(){ return 'redirect attempt blocked'; };
 
-window.addEventListener("keyup", event => {
-    if (event.ctrlKey && event.which === 192) {
-        eval(prompt("js to execute:"));
-    }
-})
+// window.addEventListener("keyup", event => {
+//     if (event.ctrlKey && event.which === 192) {
+//         eval(prompt("js to execute:"));
+//     }
+// })
 
 
